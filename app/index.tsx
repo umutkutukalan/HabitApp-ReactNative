0e961a0adbc7b0dda05af174d6c8ae52dd750051
Bahar Sarac <bbhrsrc@gmail.com>
2025-10-29 00:48:24 +0300

    add: kullanıcı kayıt sayfası eklendi

--- app/index.tsx
+++ app/index.tsx
@@ -1,51 +1,56 @@
-import { Ionicons } from "@expo/vector-icons";
-import React from "react";
 import {
-  StyleSheet,
-  Switch,
-  Text,
-  TextInput,
-  TouchableOpacity,
-  View
+    StyleSheet,
+    Text,
+    TextInput,
+    TouchableOpacity,
+    View,
 } from "react-native";
 
 export default function Index() {
-  const [isEnabled, setIsEnabled] = React.useState(false);
-
   return (
     <View style={styles.container}>
-      {/*text1*/}
-      <View
-        style={{
-          backgroundColor: "white",
-          borderRadius: 20,
-        }}
-      >
-        <Text style={styles.matruskaText}>HabitApp</Text>
-      </View>
+      <View style={styles.kullaniciKayit}>
+        {/* <View style={styles.kullaniciKayitIcon}>
+          <Ionicons name="person-add" color="#440625ff" size={50} />
+        </View>*/}
+        <Text style={styles.hosgeldinizText}>
+          {"HabitApp'e"}
+          {"\n"}Hoş Geldiniz!
+        </Text>
+        <View style={{gap:20, flexDirection: "column" }}>
+          <View style={{ gap: 16, flexDirection: "column" }}>
+            <TextInput
+              style={styles.kullaniciKayitText}
+              placeholder="Ad-Soyad"
+            ></TextInput>
+            <TextInput
+              style={styles.kullaniciKayitText}
+              placeholder="E-posta"
+            ></TextInput>
+            <TextInput
+              style={styles.kullaniciKayitText}
+              placeholder="Şifre"
+            ></TextInput>
+            <TextInput
+              style={styles.kullaniciKayitText}
+              placeholder="Şifre Tekrar"
+            ></TextInput>
+          </View>
+          <View>
+            <TouchableOpacity>
+              <Text style={styles.kullaniciKayitButon}>Kaydol</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
 
-      <View style={styles.kullanıcıGirişi}>
-        <View style={styles.kullanıcıGirişiIcon}>
-          <Ionicons name="person" color="purple" size={50} />
+        <View
+          style={{ flexDirection: "row", gap: 10, justifyContent: "center" }}
+        >
+          <Text style={{ color: "white" }}>Hesabınız var mı?</Text>
+          <TouchableOpacity>
+            <Text style={{ color: "#5fedd1ff" }}>Giriş yapın</Text>
+          </TouchableOpacity>
         </View>
-        <View style={styles.kullanıcıGirişiText}>
-          <TextInput placeholder="E-mail" />
-        </View>
-        <View style={styles.kullanıcıGirişiText}>
-          <TextInput placeholder="Şifre" />
-        </View>
-        <View style={styles.kullanıcıGirişiSwitch}>
-          <Switch
-            value={isEnabled} // Switch’in mevcut durumu (true/false)
-            onValueChange={setIsEnabled} // Tıklanınca değeri değiştirir
-            trackColor={{ false: "#afadadff", true: "#5fedd1ff" }} // Arka plan rengi
-            thumbColor="white" // Küçük dairenin rengi
-          />
-          <Text style={{color:"white"}}>Beni hatırla</Text>
-        </View>
-        <TouchableOpacity>
-          <Text style={styles.kullanıcıGirişiButton}>Giriş</Text>
-        </TouchableOpacity>
       </View>
     </View>
   );
@@ -54,24 +59,20 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "flex-start",
-    alignItems: "flex-start",
-    backgroundColor: "purple",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#440625ff",
     padding: 10,
   },
-  matruska: {
-    backgroundColor: "white",
-    padding: 10,
-    flexDirection: "column",
-    gap: 10,
-    borderRadius: 20,
+  kullaniciKayit: {
+    gap: 20,
   },
-  matruskaText: {
-    fontSize: 20,
-    padding: 10,
-    color: "black",
+  hosgeldinizText: {
+    fontSize: 28,
+    fontWeight: "bold",
+    color: "#5fedd1ff",
   },
-  kullanıcıGirişiIcon: {
+  /*kullaniciKayitIcon: {
     width: 80,
     height: 80,
     backgroundColor: "white",
@@ -79,37 +80,22 @@ const styles = StyleSheet.create({
     alignItems: "center",
     borderRadius: 40,
     marginLeft: 50,
+  },*/
+  kullaniciKayitText: {
+    width: 200,
+    backgroundColor: "white",
+    padding: 10,
+    borderRadius: 10,
   },
-  kullanıcıGirişi: {
+  kullaniciKayitButon: {
+    width: 100,
+    marginLeft: 50,
+    backgroundColor: "#5fedd1ff",
+    fontSize: 16,
+    fontWeight: "bold",
+    color: "#440625ff",
     padding: 10,
     borderRadius: 20,
-    gap: 16,
-    width: 200,
-    marginTop: 100,
-    marginLeft: 100,
-  },
-  kullanıcıGirişiText: {
-    backgroundColor: "white",
-    fontSize: 16,
-    paddingHorizontal: 10,
-    borderWidth: 1,
-    borderRadius: 20,
-    borderColor: "white",
-  },
-  kullanıcıGirişiSwitch: {  
-    flexDirection: "row",
-    justifyContent: "flex-start",
-    alignItems: "center",
-    gap: 15,
-
-  },
-  kullanıcıGirişiButton: {
-    width: 60,
-    marginLeft: 60,
-    backgroundColor: "white",
-    fontSize: 16,
     textAlign: "center",
-    paddingVertical: 8,
-    borderRadius: 20,
   },
 });
